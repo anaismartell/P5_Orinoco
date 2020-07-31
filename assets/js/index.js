5dec76cd60fba94630e1334ee65b787fd8022b92
anaismartell <martell.anais@gmail.com>
2020-07-31 16:33:57 +0200

    modif commentaires + élaboration étape fichier product.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,35 @@
-
 let container = document.getElementById("teddies_list") // Création d'une variable container qui correspond à la div teddies_list du fichier HTML
 let url = "http://localhost:3000/api/teddies" 
 
 // Récupération des données de l'API grâce à fetch
 fetch(url)
-    .then(response => response.json()) // Retourner la réponse au format js
-    .then(json =>{
+    .then(teddies => teddies.json())// Retourner une promesse contenant en réponse l'objet teddies au bon format
+    .then(teddies =>{
         // Création de la boucle pour récupérer chaque élément
-        json.forEach(({_id, name, description, price, imageUrl}) =>{
-            // Etape 1: Création des nouveauxx éléments à l'aide de createElementou createTextNode
+        teddies.forEach(({_id, name, description, price, imageUrl}) =>{
+            // Etape 1: Création des nouveaux éléments à l'aide de createElement ou createTextNode
             let div = document.createElement("div") 
             let img = document.createElement("img") 
             let h3 = document.createElement("h3") 
             let h4 = document.createElement("h4") 
             let p = document.createElement("p") 
-            let link = document.createElement("a") 
+            let a = document.createElement("a") 
             let apiName = document.createTextNode (name) // Création d'une variable qui stockera le nom de l'ourson ce dernier correspondant à la donnée "name" de l'API
             let apiPrice = document.createTextNode (price/100+ " €") // Création d'une variable qui stockera le prix de l'ourson ce dernier correspondant à la donnée "price" de l'API
             let apiDescription = document.createTextNode (description) // Création d'une variable qui stockera la description de l'ourson cette dernière correspondant à la donnée "description" de l'API
 
             // Etape 2: Définition des informations des éléments
-            link.href = 'product.html?id=' + _id; 
-            link.textContent = "Voir le produit"
+            a.href = 'product.html?id=' + _id; 
+            a.textContent = "Voir le produit"
             img.src = imageUrl
 
-            // Etape 3: Insertion des nouveaux éléments dans le DOM
+            // Etape 3: Insertion des nouveaux éléments dans le DOM grâce à la méthode appendChild
             container.appendChild (div) 
             div.appendChild (img) 
             div.appendChild (h3) 
             div.appendChild (h4) /
             div.appendChild (p) 
-            div.appendChild (link) 
+            div.appendChild (a) 
             h3.appendChild(apiName) // Le h3 ajouté doit correspondre au nom de l'ourson 
             h4.appendChild(apiPrice) // Le h4 ajouté doit correspondre au prix de l'ourson
             p.appendChild(apiDescription) // Le paragraphe ajouté doit correspondre à la description de l'ourson
@@ -39,7 +38,7 @@ fetch(url)
             container.className="d-flex flex-wrap justify-content-center lg-flex-row sm-flex-column mx-auto"
             div.className ="card m-5 border-dark col-10 offset-1 col-md-4 col-lg-3 bg-dark text-white"
             img.className ="card-img-top my-3"
-            link.className = "btn btn-info offset-6 m-3"
+            a.className = "btn btn-info offset-6 m-3"
         })
     })
     .catch(function (error) {
---
+++ assets/js/product.js
@@ -0,0 +1,47 @@
+// Récupérer la div HTML 
+
+// Récupérer l'ID du produit dans l'URL pour l'afficher dans la page 
+
+// Comment afficher un seul produit en fonction de l'ID? 
+
+// Créer les éléments de la page 
+    // Div
+    // Image 
+    // Nom
+    // Prix
+    // Description
+    // Couleurs
+        // Créer une liste déroulante avec les choix des couleurs
+    // Créer un bouton "Ajouter au panier"
+
+
+// Créer un objet modèle et définir les éléments
+    // Récupérer l'info prix 
+    // Récupérer l'info image
+    // Récupérer l'info nom 
+    // Récupérer l'info description
+    // Récupérer les couleurs (dans un tableau? + boucle pour les intégrer) 
+    
+
+// Insérer les éléments dans le DOM
+
+
+// Appliquer les styles Boostrap
+
+
+
+//*********** NON VISIBLE SUR LA PAGE PRODUIT MAIS VISIBLE DANS LA PAGE PANIER ************
+
+
+// Créer un tableau d'objets dans la page panier
+
+// Créer une fonction qui ajoute un objet contenan toutes les infos dans la page panier
+
+// Créer une fonction affichant le nombre de produit dans le panier
+
+// Créer une fonction qui ajoutera +1 dans la barre de navigation
+
+// Créer une fonction qui calcule le prix total dans le panier
+
+// Créer un événement (addEventListener) au moment du clic sur le bouton "Ajouter au panier" (devra appeler les fonctions précédentes)
+
